62bfae6e77502d43ceaf5a0b143a7e350b387268
yotam21yotam <58113150+yotam21yotam@users.noreply.github.com>
2024-07-04 22:36:37 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,12 +6,60 @@ qa_pipeline = pipeline("question-answering", model="distilbert-base-uncased-dist
 
 # Personal context about you
 context = """
-I am Yotam, a data scientist and machine learning engineer with expertise in cybersecurity. 
-I graduated with an M.Sc in Computer Science, specializing in Intelligent Systems. I have worked 
-on various projects, including the development of sophisticated algorithms for predictive analytics 
-and real-time data processing systems. In my free time, I enjoy hiking, photography, and working on 
-open-source projects.
-Ilan Braverman do not know how to ride motorcycles, he can only ride bicycles with supprting wheels...
+I am Yotam, a Data Scientist with a robust background in Statistics, Machine Learning engineering, 
+and cybersecurity. I have built ML pipelines and led projects from proof of concept (POC) to 
+production across various organizations, enhancing their ability to leverage data effectively. 
+My diverse experience has equipped me with a unique blend of skills, making me a quick learner and 
+a creative problem solver who brings insightful and innovative solutions to complex challenges.
+
+Key Details:
+- Age: I am 36 years old.
+- Email: You can contact me at yotam21@gmail.com.
+- Residence: I live in Tel Aviv.
+- Current Position: I am currently working at the Prime Minister's Office as a Data Scientist 
+  specializing in cybersecurity.
+- Expertise: My expertise primarily lies in computer networks and operating systems.
+- Hobbies: I enjoy extreme sports like motocross and hill climbs.
+- Military Service: I served in the army as a fighter for 3 years.
+
+Professional Experience:
+From 2021 to Present:
+- Position: Data Scientist & ML Engineer
+- Organization: Prime Minister's Office, Tel Aviv
+- Responsibilities: Conducted R&D for ML-driven Intrusion Detection Systems (IDS), with a deep 
+  understanding of the OSI model and operating systems implemented with Python, C, Zeek, and Splunk. 
+  Led research and development efforts on TLS fingerprinting tools and built an MLOps infrastructure 
+  to support CI/CD/CT for a diverse range of projects, include data collection and preparation, model 
+  development and training, ML service deployment, continuous feedback, and monitoring using MLflow, 
+  Airflow, Docker, Kubernetes, S3, Jenkins, and Kafka.
+
+From 2020 to 2021:
+- Position: Data Scientist
+- Organization: Nogamy, Tel Aviv
+- Projects:
+  BioRad - Developed and deployed ML driven projects for predictive maintenance and fault prediction 
+  on biological equipment, reducing equipment malfunctions by over 80% using Python and SQL.
+  InTelos - Developed and deployed ML driven projects for BI and fraud detection, leveraging network 
+  data using Python and SQL.
+  Golan Telecom - Conducted R&D on ETL processes to reduce time and space complexities, achieving over 
+  a 20% reduction in running time using Python and SQL.
+
+From 2019 to 2020:
+- Position: Data Scientist
+- Organization: eLoomina, Tel Aviv
+- Achievements: Designed and developed statistical models predicting human behavior based on large 
+  datasets using R, Python, and SQL. Successfully Implemented ML models that increased the accuracy 
+  of behavioral predictions. Engineered and refined data architectures that supported complex data 
+  operations, significantly reducing latency.
+
+Technical Skills:
+Statistics, Deep Learning, Machine Learning, Signal Processing, System Design, Predictive Modeling, 
+Spark, Splunk, Docker, Linux, GIT, Kubernetes, Grafana, MongoDB, Kafka, Jenkins, Apache, IDS, Snort, 
+Zeek, Suricata, Wireshark, tshark, Nmap, TCP/IP, OSI model, AWS, Azure, Snowflake, Databricks, S3, 
+Python, R, C, SQL, MLflow, Airflow, TensorFlow, Keras, Pandas, scikit-learn, Gensim, Matplotlib, 
+Seaborn, NumPy, PyTorch, Dask, Scapy, PyCaret, Transformers, PySpark, Boto3, Joblib.
+
+
 """
 
 st.title('About Me Chatbot')
